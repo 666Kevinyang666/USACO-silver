40049ebc792c954d88990d4a6a49f830263c3a2c
666Kevinyang666 <84572815+666Kevinyang666@users.noreply.github.com>
2021-08-06 21:10:56 +0800

    Create berries.py

---
+++ berries.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('berries.in','r')
+sys.stdout = open('berries.out','w')
+n, k = map(int, input().split())
+fruits = list(map(int, input().split()))
+ans = 0
+for i in range(1,1001):
+    a = 0  
+    for j in range(n):
+        a+=fruits[j]//i
+    if a<k//2:
+        break
+    if a>=k:
+        ans = max(ans,i*k//2)
+        continue
+    fruits.sort(revers = True,key = lambda x:x%i)
+    temp = (a-k//2)*i
+    for j in range(k-a):
+        if j>n-1:
+            break
+        temp+=fruits[j]%i
+    ans = max(ans,temp)
+print(ans)
